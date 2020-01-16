041c1fecd05ca2350d89a5808b0564b37f52adaa
Aleksuo <aleksi.suoranta@gmail.com>
2020-01-16 16:05:42 +0200

    changed ip to ssh command

--- src/commands.py
+++ src/commands.py
@@ -3,9 +3,9 @@ from requests import get
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-def ip(update,context):
+def ssh(update,context):
     ip = get('https://api.ipify.org').text
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Current public IP address is: "+ip)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="ssh pi@"+ip+":50000")
 
 def test(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="BEEP!BEEP!")
